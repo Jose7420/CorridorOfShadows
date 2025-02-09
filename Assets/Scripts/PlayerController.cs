02e53317fbdf4ef3cce3d220fbf1d50a62e8cf1b
Jose J. Cabezas <79861010+Jose7420@users.noreply.github.com>
2025-02-09 14:38:37 +0100

    modificado playerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,16 +7,22 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     PlayerInput _playerInput;
+    Rigidbody2D _RigidbodyPlayer;
     private Vector2 movimiento;
     private float jump;
 
-    [SerializeField] float _speed;
-    [SerializeField] float _jumpForce;
+    [SerializeField] float _speed = 10f;
+    [SerializeField] float _jumpForce = 0f;
     private bool _IsGround;
+
+    [SerializeField] private float minX = -17f;
+    [SerializeField] private float maxX = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
         _playerInput = GetComponent<PlayerInput>();
+        _RigidbodyPlayer = GetComponent<Rigidbody2D>();
         _speed = 10f;
 
     }
@@ -24,41 +30,47 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        movimiento = _playerInput.actions["Movimientos"].ReadValue<Vector2>();
+        jump = _playerInput.actions["Jump"].ReadValue<float>();
+
     }
 
     private void FixedUpdate()
     {
-        movimiento = _playerInput.actions["Movimientos"].ReadValue<Vector2>();
-        // jump = _playerInput.actions["Jump"].ReadValue<float>();
-        // print(jump);
-        
-        jump = _playerInput.actions["Jump"].ReadValue<float>();
-        Debug.Log($"Return {jump}");
 
-         _jumpForce = 0;
-
-         if (jump == 1 && _IsGround)
-         {
-             _IsGround = false;
-             _jumpForce = 100f;
-             Debug.Log("Dentro del condicional " + _IsGround);
-         }
-
-         
-
-      
-
-
-
-        transform.Translate(movimiento.x * _speed * Time.fixedDeltaTime,
-            (jump * _speed  + _jumpForce) * Time.fixedDeltaTime, 0);
+        Mover();
+        Jump();
     }
-   
+
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        //Debug.Log("suelo");
         _IsGround = collision.gameObject.CompareTag("Ground");
-       // Debug.Log(_IsGround);
+        Debug.Log($"Dentro del collision {_IsGround}");
     }
+
+
+
+    private void Jump()
+    {
+
+        _jumpForce = 0;
+
+        if (jump == 1 && _IsGround)
+        {
+            _IsGround = false;
+            _jumpForce = 10f;
+
+            _RigidbodyPlayer.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+            Debug.Log("Dentro del condicional " + _IsGround);
+        }
+
+    }
+
+    private void Mover()
+    {
+
+        _RigidbodyPlayer.transform.Translate(Mathf.Clamp(movimiento.x * _speed * Time.fixedDeltaTime, minX, maxX), 0, 0);
+    }
+
 }
