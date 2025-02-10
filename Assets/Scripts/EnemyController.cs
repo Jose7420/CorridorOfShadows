1b24a94d33e7441cd53f78f98907a65eb3631105
Jose J. Cabezas <7962446@alu.murciaeduca.es>
2025-02-10 13:26:55 +0100

    add spawnerManager

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private Rigidbody2D _RigidbodyPlayer;
+
+    [Header("Velocidad del enemigo")] [SerializeField]
+    private float speed = 50f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _RigidbodyPlayer = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _RigidbodyPlayer.AddForce(Vector2.left * speed * Time.deltaTime, ForceMode2D.Impulse);
+
+        if (transform.position.x <= -17.33)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnerManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerManager : MonoBehaviour
+{
+    [SerializeField] private GameObject[] enemigos;
+    private float positionY;
+    private float positionX;
+    private float offset = 0.5f;
+    [SerializeField,Range(1,5)] private int numeroDeEnemigos;
+    private float temporizador = 0.5f;
+    private int index;
+    
+    private Dictionary<int,string> colors = new Dictionary<int,string>()
+    {
+        { 0,"red"},
+        { 1,"green"},
+        { 2,"blue"},
+        { 3,"yellow"},
+        { 4,"purple"},
+        { 5,"orange"},
+        { 6,"pink"},
+        { 7,"purple"},
+        { 8,"orange"},
+        { 9,"pink"},
+        { 10,"purple"},
+        
+    };
+   
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //numeroDeEnemigos = 1;
+        index = enemigos.Length;
+        
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        temporizador -= Time.deltaTime;
+        if (temporizador < 0  )
+        {
+            for(int i = 0; i < numeroDeEnemigos; i++)
+            {
+                positionY =Random.Range(-2.8f, 4.84f);
+                positionX = Random.Range(transform.position.x, transform.position.x + offset);
+                index= Random.Range(0,enemigos.Length);
+      
+                Instantiate(enemigos[index], new Vector3(transform.position.x,positionY, transform.position.z), enemigos[index].transform.rotation);
+               
+            }
+
+            temporizador = 1f;
+        }
+    }
+}
