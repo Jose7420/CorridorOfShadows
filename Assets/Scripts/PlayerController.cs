0c295ccf4066e20a21ca8eaca178c70b6ff10904
Jose J. Cabezas <7962446@alu.murciaeduca.es>
2025-02-22 22:17:09 +0100

    pequeñas modificaciones
    add animation player, el sprite es provisional hasta que se haga el definitivo

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,19 +14,24 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float _speed;
     [SerializeField] float _jumpForce;
     private bool _IsGround;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
 
-   [SerializeField] private float minX = -15.7f;
+    [SerializeField] private float minX = -15.7f;
     [SerializeField] private float maxX = 74f;
+
 
     // Start is called before the first frame update
     void Start()
     {
         _playerInput = GetComponent<PlayerInput>();
         _RigidbodyPlayer = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+
         // _speed = 10f;
         _speed = 500;
         _jumpForce = 10f;
-
     }
 
     // Update is called once per frame
@@ -39,24 +44,48 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-
         Mover(direction.x * _speed * Time.fixedDeltaTime);
-       
     }
 
+    #region Collisiones
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         _IsGround = collision.gameObject.CompareTag("Ground");
-//        Debug.Log($"Dentro del collision {_IsGround}");
+
+    }
+    #endregion
+
+    #region Movimiento player
+
+    /// <summary>
+    /// Mover al player cuando se pulse los botones de direcion
+    /// </summary>
+    /// <param name="mover"></param>    
+    private void Mover(float mover)
+    {
+        if (_IsGround)
+        {
+            ChangeFlip(direction.x);
+            animator.SetBool("isStatic", IsStatic(direction.x));
+
+            _RigidbodyPlayer.velocity = new Vector2(mover, _RigidbodyPlayer.velocity.y);
+
+            _RigidbodyPlayer.position = new Vector2(Mathf.Clamp(_RigidbodyPlayer.position.x, minX * 1f, maxX),
+                _RigidbodyPlayer.position.y);
+        }
+        Jump();
+
+
     }
 
 
-
+    /// <summary>
+    /// Comprobar si se a pulsado la tecla o el boton de salto
+    /// y si esta en tierra para saltar
+    /// </summary>
     private void Jump()
     {
-
-        // _jumpForce = 0;
 
         // Comprobar si esta en el suelo y el salto es hacia arriba
         if (jump == 1 && _IsGround)
@@ -65,22 +94,43 @@ public class PlayerController : MonoBehaviour
             // _jumpForce = 10f;
 
             _RigidbodyPlayer.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
-           // Debug.Log("Dentro del condicional " + _IsGround);
+            animator.SetBool("isStatic", IsStatic(direction.y));
+            // Debug.Log("Dentro del condicional " + _IsGround);
         }
 
     }
+    #endregion
 
-    private void Mover(float mover)
+    #region Cambiar de flip
+
+    /// <summary>
+    /// Cambiar la direccion del sprite
+    /// se manda la direcion por parametros el valor de tipo float donde quiere que mire el sprite
+    /// </summary>
+    /// <param name="direction"></param>
+    private void ChangeFlip(float direction)
     {
 
-        _RigidbodyPlayer.velocity = new Vector2(mover, _RigidbodyPlayer.velocity.y);
-
-        _RigidbodyPlayer.position = new Vector2(Mathf.Clamp(_RigidbodyPlayer.position.x, minX * 1f, maxX),
-            _RigidbodyPlayer.position.y);
-        Jump();
-
+        if (direction == 0) return;
+        spriteRenderer.flipX = direction < 0;
 
     }
+    #endregion
 
- 
+    #region Ver el estado istatic
+
+    /// <summary>
+    /// Comprobar si el player esta parado
+    /// se mando por parametro la direcion si es cero devuelve true.
+    /// </summary>
+    /// <param name="direction"></param>
+    /// <returns></returns>
+    private bool IsStatic(float direction)
+    {
+        return direction == 0;
+
+    }
+    #endregion
+
+
 }
