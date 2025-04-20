2e65c5b39871cb672c34c1f527f18b905792b6e9
Jose J. Cabezas <79861010+Jose7420@users.noreply.github.com>
2025-04-20 20:56:22 +0200

    add animacion de down

--- Assets/Scripts/PlayerControllerCOS.cs
+++ Assets/Scripts/PlayerControllerCOS.cs
@@ -13,7 +13,7 @@ public class PlayerControllerCOS : MonoBehaviour
     private float _jump;
 
     [SerializeField] private float _speed = 500;
-    [SerializeField] private float _jumpForce = 12f;//10f;
+    [SerializeField] private float _jumpForce = 14.5f;//12f;//10f;
     private bool _isGrounded;
     private SpriteRenderer _spriteRenderer;
     private Animator _animator;
@@ -21,15 +21,17 @@ public class PlayerControllerCOS : MonoBehaviour
     [SerializeField] private float minX = -15.7f;
     [SerializeField] private float maxX = 114.5f; //74f;
 
-    [SerializeField] private float _damage;
+    [SerializeField] private float _damage = 10;
     [SerializeField] private float _life = 50;
     [SerializeField] private AudioClip _audioClip;
     private enum Jumping : ushort { up = 1 };
     private bool _hitPressed;
+    private bool _downPressed;
     [SerializeField] private ParticleSystem dustFoot;
 
     private CombatBodyToBody _handToHandCombat;
-
+    [SerializeField] private float _posicionYPlayerDown = -1.44f;
+    [SerializeField] private float _posicionYPlayer = -0.2f;
 
     // Start is called before the first frame update
     void Start()
@@ -48,21 +50,29 @@ public class PlayerControllerCOS : MonoBehaviour
     {
 
         _hitPressed = _playerInput.actions["Golpe"].IsPressed();
+        _downPressed = _playerInput.actions["Down"].IsPressed();
 
         _handToHandCombat.TimeBetweenAttack();
 
         if (_hitPressed)
         {
+            _animator.SetBool("Down", false);
             _rigidbodyPlayer.velocity = Vector2.zero;
             _handToHandCombat.Stroke();
         }
-        else if(!_handToHandCombat.IsActiveHit)
+        else if (!_handToHandCombat.IsActiveHit)
         {
-            _direction = _playerInput.actions["Movimientos"].ReadValue<Vector2>();
-            _jump = _playerInput.actions["Jump"].ReadValue<float>();
-
+           
+                _direction = _playerInput.actions["Movimientos"].ReadValue<Vector2>();
+                _jump = _playerInput.actions["Jump"].ReadValue<float>();
+            
+            
 
         }
+
+
+
+
 
     }
 
@@ -71,12 +81,13 @@ public class PlayerControllerCOS : MonoBehaviour
 
 
 
-
         Debug.Log($"direcion x{_direction.x} y {_jump} ");
         if (!_handToHandCombat.IsActiveHit)
         {
 
             MovePlayer(_direction.x * _speed * Time.fixedDeltaTime);
+
+
         }
 
 
@@ -122,22 +133,28 @@ public class PlayerControllerCOS : MonoBehaviour
             Debug.Log($"La variable _direction es {_direction}.");
 
             FlipSprite(_direction.x);
-            //Debug.Log($"mover player {move}"); 
+            Debug.Log($"mover player {move}");
             // dustFoot.Play();
 
-
+            IsDown();
+           
 
             _animator.SetBool("isStatic", IsPlayerStatic(_direction.x));
             _animator.SetBool("jump", false);
-            if(_direction.x != 0)  {dustFoot.Play();}
 
-            if(!_handToHandCombat.IsActiveHit){
-            _rigidbodyPlayer.velocity = new Vector2(move, _rigidbodyPlayer.velocity.y);
-            _rigidbodyPlayer.position = new Vector2(Mathf.Clamp(_rigidbodyPlayer.position.x, minX, maxX),
-                _rigidbodyPlayer.position.y);
+            if (_direction.x != 0) { dustFoot.Play(); }
+
+            if (!_handToHandCombat.IsActiveHit && !_downPressed)
+            {
+                _rigidbodyPlayer.velocity = new Vector2(move, _rigidbodyPlayer.velocity.y);
+                _rigidbodyPlayer.position = new Vector2(Mathf.Clamp(_rigidbodyPlayer.position.x, minX, maxX),
+                    _rigidbodyPlayer.position.y);
 
             }
+
+
         }
+
 
         JumpPlayer(move);
 
@@ -150,11 +167,11 @@ public class PlayerControllerCOS : MonoBehaviour
     /// </summary>
     private void JumpPlayer(float move)
     {
-        if (_jump == 1 && _isGrounded)
+        if (_jump == 1 && _isGrounded )
         {
 
             _isGrounded = false;
-
+            _animator.SetBool("Down", false);
             // Para la velocidad del player
             _rigidbodyPlayer.velocity = Vector2.zero;
 
@@ -168,6 +185,25 @@ public class PlayerControllerCOS : MonoBehaviour
         }
     }
 
+    private void IsDown()
+    {
+        if (_isGrounded && IsPlayerStatic(_direction.x))
+        {
+            bool isDown = _downPressed;
+            _rigidbodyPlayer.velocity = Vector2.zero;
+            if (isDown) { PlayerPosition(); }
+
+            _animator.SetBool("Down", isDown);
+
+        }
+
+
+    }
+
+    public void PlayerPosition()
+    {
+        transform.position = new Vector3(transform.position.x, _posicionYPlayerDown, transform.position.z);
+    }
     #endregion
 
     #region Cambiar de flip
